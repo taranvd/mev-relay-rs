0275fc45b225480c46914dbc909fac491259b186
Vladyslav Taran <apple.vlad.taran@gmail.com>
2026-07-08 18:49:56 +0300

    feat(app): add CLI, composition root, health check, and graceful shutdown

--- crates/health/src/lib.rs
+++ crates/health/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use warp::Filter;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn serve_health(port: u16) {
+    let route =
+        warp::path("health").map(|| warp::reply::with_status("ok", warp::http::StatusCode::OK));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    warp::serve(route).run(([0, 0, 0, 0], port)).await;
 }
